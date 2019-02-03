6400eb19398e21d29e6bea5b418940bb303dcb38
bluetechnology <takanori.fukuyama@ngl.technology>
2019-02-03 19:34:53 +0900

    access local strage

--- src/App.js
+++ src/App.js
@@ -1,56 +1,100 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDoListItem from './ToDoListItem.js';
+import ToDoListItem from "./ToDoListItem.js"
 
 class App extends Component {
 
+  // ToDoListをstateに定義、初期値はlocalStorageから取得または []
   state = {
-    todoList: []
+    todoList: JSON.parse(localStorage.getItem("todoList")) || []
+  }
+
+  // todoList itemの追加
+  addTodo = (item, callBack) => {
+    // todoList stateに追加
+    this.setState(
+      {
+        todoList: this.state.todoList.concat(item)
+      },
+      () => {
+        // localStorageにtodoList stateを保存
+        localStorage.setItem("todoList", JSON.stringify(this.state.todoList))
+        // callBack関数が引数に渡されていた場合に実行
+        callBack && callBack()
+      }
+    )
+  }
+
+  // todoListからitemを削除
+  removeTodo = (item, callBack) => {
+    this.setState(
+      {
+        todoList: this.state.todoList.filter(x => x !== item)
+      },
+      () => {
+        // localStorageにtodoList stateを保存
+        localStorage.setItem("todoList", JSON.stringify(this.state.todoList))
+        // callBack関数が引数に渡されていた場合に実行
+        callBack && callBack()
+      }
+    )
   }
 
   render() {
     return (
       <div className="App">
-        <form 
+        <form
           className="App-form"
-          onSubmit={e =>{
+          onSubmit={e => {
+            // formのデフォルトのイベントをキャンセル
             e.preventDefault();
-            const titleElement = e.target.elements["title"];
+
+            // idがtitleのElementを取得
+            const titleElement = e.target.elements["title"]
+            // idがdescriptionのElementを取得
             const descriptionElement = e.target.elements["description"];
-            this.setState(
+
+            this.addTodo(
               {
-                todoList: this.state.todoList.concat({
-                  title: titleElement.value,
-                  description: descriptionElement.value
-                })
+                title: titleElement.value,
+                description: descriptionElement.value
               },
               () => {
+                // stateの変更後に入力した値を空にする
                 titleElement.value = "";
                 descriptionElement.value = "";
               }
             )
           }}
-          >
+        >
           <div>
-            <input id="title" placeholder="title"/>
-            <textarea id="description" placeholder="description"/>
+            <input
+              id="title"
+              placeholder="title"
+            />
+            <textarea
+              id="description"
+              placeholder="description"
+            />
           </div>
           <div>
-            <button type="submit">登録</button>
+            <button
+              type="submit"
+            >
+              登録
+            </button>
           </div>
         </form>
         <div>
-          {/* todoList Arrayの要素分のコンポーネントを展開 */}
+        {/* todoList配列の要素数分ToDoListItemコンポーネントを展開 */}
           {this.state.todoList.map(todo => (
-            <ToDoListItem key={todo.title} 
-             title={todo.title} 
-             description={todo.description} 
-             onClick={() => {
-               this.setState({
-                 todoList: this.state.todoList.filter(x => x !== todo)
-               })
-             }}
-             />
+            <ToDoListItem
+              key={todo.title}
+              title={todo.title}
+              description={todo.description}
+              // クリックされたItemをtodoList stateから削除
+              onClick={() => this.removeTodo(todo)}
+            />
           ))}
         </div>
       </div>
